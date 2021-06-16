22d6447e4db7ca2ed4761853ad1032b682f7a82c
anitaRead <areadman91@gmail.com>
2021-06-16 14:53:55 +0100

    Initial commit

---
+++ app.rb
@@ -0,0 +1,16 @@
+require "sinatra"
+require "sinatra/reloader" if development?
+
+class Battle < Sinatra::Base
+  configure :development do
+    register Sinatra::Reloader
+  end
+
+  get '/' do
+    'Hello Battle!'
+  end
+
+end
+
+#   run! if app_file == $0
+
