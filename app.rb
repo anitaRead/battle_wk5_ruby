5588df4b4d5c499220568136253396660997fb5a
anitaRead <areadman91@gmail.com>
2021-06-17 15:06:59 +0100

    Add form so players can enter their names

--- app.rb
+++ app.rb
@@ -7,7 +7,14 @@ class Battle < Sinatra::Base
   end
 
   get '/' do
-    'Testing infrastructure working!'
+    # 'Testing infrastructure working!'
+    erb :index
+  end
+
+  post '/start-game' do
+    @player1 = params[:player1]
+    @player2 = params[:player2]
+    erb :start
   end
 
   run! if app_file == $0
---
+++ spec/features/name_form_spec.rb
@@ -0,0 +1,9 @@
+feature 'Testing name form' do
+    scenario 'can fill in form and submit names' do
+        visit('/')
+        fill_in('player1', with: 'Anita')
+        fill_in('player2', with: 'Juel')
+        click_button('submit')
+        expect(page).to have_content "Anita vs Juel"
+    end
+end
